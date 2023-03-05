6450162ddb9da29303f26b39943e8e00f871430d
Cavit Ertuğrul Sırt <ertugrul.sirt@sipay.com.tr>
2023-03-05 15:37:40 +0300

    First solution

--- scripts/actions/issue-pr.ts
+++ scripts/actions/issue-pr.ts
@@ -264,7 +264,7 @@ function getTimestampBadge() {
 
 export function getQuestionFullName(no: number, difficulty: string, title: string) {
   return `${String(no).padStart(5, '0')}-${difficulty}-${slug(
-    title.replace(/\./g, '-').replace(/<.*>/g, ''),
+    (title || "").replace(/\./g, '-').replace(/<.*>/g, ''),
     { tone: false },
   )}`
 }
